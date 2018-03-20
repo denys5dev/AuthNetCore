6ac7a1a59f2bcb8180b18fe103284b7a4d13f1e1
denys5dev <denys5dev@gmail.com>
2018-03-20 23:49:35 +0100

    refactoring

--- ClientApp/app/routes/routes.module.ts
+++ ClientApp/app/routes/routes.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-// import { TranslatorService } from '../core/translator/translator.service';
-// import { MenuService } from '../core/menu/menu.service';
+
 import { SharedModule } from '../shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 
-// import { menu } from './menu';
 import { routes } from './routes';
 import { AuthService } from '../core/auth.service';
 
@@ -23,7 +21,5 @@ import { AuthService } from '../core/auth.service';
 })
 
 export class RoutesModule {
-    // constructor(public menuService: MenuService, tr: TranslatorService) {
-    //     menuService.addMenu(menu);
-    // }
+  
 }
